d6c2c8f612730f4dad689a044d82e01deacc03ad
Sandra Nwakeze <sandramakis.dev@gmail.com>
2023-11-24 01:40:47 +0100

    RM: write up, ADD: To-do event

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-// Your javascript goes here
+"use strict";
+
+const todos = document.querySelectorAll(".to-dos .to-do"),
+  writeups = document.querySelectorAll(".write-up");
+
+let isClicked = false;
+
+function displayTodo(e) {
+  todos.forEach((todo, idx) => {
+    // const currentInd = todo.classList.add(`${[idx]}`);
+    // currentInd.classList.add("display");
+    // todo.style.display = "flex";
+    // console.log(todo, idx);
+    // return console.log(currentInd);
+  });
+}
+
+todos.forEach((todo) => todo.addEventListener("click", displayTodo));
