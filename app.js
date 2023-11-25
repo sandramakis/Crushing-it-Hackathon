788c9921bb611faafa2ec42e01849bf6fe6dc6f2
Sandra Nwakeze <sandramakis.dev@gmail.com>
2023-11-25 23:32:06 +0100

    ADD: Display menu, ADD: Arrow down button

--- app.js
+++ app.js
@@ -3,16 +3,30 @@
 const todos = document.querySelectorAll(".to-dos .to-do"),
   writeups = document.querySelectorAll(".write-up");
 
+const arrowDown = document.querySelector("#arrow-down");
+const todoParent = document.querySelector("#to-dos");
+const arrowUp = document.querySelector("#arrow-up");
+
+arrowDown.addEventListener("click", function () {
+  const isExpanded = arrowDown.attributes["aria-expanded"].value;
+
+  todoParent.classList.toggle("menu-display");
+  arrowUp.style.display = "block";
+  // arrowDown.style.display = "none";
+
+  if (isExpanded) {
+    arrowDown.ariaExpanded = "false";
+  } else {
+    arrowDown.ariaExpanded = "true";
+  }
+});
+
 let isClicked = false;
 
-function displayTodo(e) {
-  todos.forEach((todo, idx) => {
-    // const currentInd = todo.classList.add(`${[idx]}`);
-    // currentInd.classList.add("display");
-    // todo.style.display = "flex";
-    // console.log(todo, idx);
-    // return console.log(currentInd);
-  });
-}
+// function displayTodo(e) {
+//   todos.forEach((todo, idx) => {
+
+//   });
+// }
 
 todos.forEach((todo) => todo.addEventListener("click", displayTodo));
